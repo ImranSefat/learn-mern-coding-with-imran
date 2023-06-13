3caeccaeae6f93991e27bf665529790e9bbfce2c
Imran Sefat <imransefat@gmail.com>
2023-06-14 01:40:32 +0600

    fix: starter template fixed with assets

--- nasa-astronomy/src/App.tsx
+++ nasa-astronomy/src/App.tsx
@@ -1,7 +1,10 @@
 export default function App() {
   return (
     <div className="h-screen flex justify-center items-center bg-slate-800 text-slate-50">
-      <h1 className="text-5xl">Welcome to the first day of the workshop!</h1>
+      <div className="flex flex-col items-center gap-12">
+        <img src="/images/sample4.png" alt="" className="max-w-lg" />
+        <h1 className="text-5xl">Welcome to the first day of the workshop!</h1>
+      </div>
     </div>
   );
 }
