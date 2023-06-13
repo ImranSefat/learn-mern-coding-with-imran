f0d9eed2e63f4ad866f2ae618662eeff2fe75414
Imran Sefat <imransefat@gmail.com>
2023-06-14 01:31:13 +0600

    feat: starter template added with tailwind, typescript

---
+++ nasa-astronomy/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-slate-800 text-slate-50">
+      <h1 className="text-5xl">Welcome to the first day of the workshop!</h1>
+    </div>
+  );
+}
